66b1ef6e0f05d2d4708cf381d5cecabecde4e732
vilemgrim <vilem.grim@gmail.com>
2026-04-14 13:37:25 +0200

    Základní nastavení Spring Boot, Reactu a databáze

---
+++ src/main/java/cz/osu/team25/swiproject/SwiProjectApplication.java
@@ -0,0 +1,13 @@
+package cz.osu.team25.swiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/osu/team25/swiproject/SwiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.team25.swiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
