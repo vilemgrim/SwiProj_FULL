0a57aa0b506dd2570dee3ac535c31ef7019bdd0e
vilemgrim <vilem.grim@gmail.com>
2026-04-14 16:38:15 +0200

    Backend: Implementace Question modelu, repository, service a controlleru

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO question (text, option1, option2, option3, option4, correct_answer_index, category)
+VALUES ('Jak se jmenuje maskot Javy?', 'Rex', 'Duke', 'Tux', 'Moby', 2, 'Java');
+
+INSERT INTO question (text, option1, option2, option3, option4, correct_answer_index, category)
+VALUES ('Který rok vznikla Java?', '1991', '1995', '1998', '2000', 2, 'Java');
+
+INSERT INTO question (text, option1, option2, option3, option4, correct_answer_index, category)
+VALUES('Která společnost vlastní Javu?', 'Microsoft', 'Roblox', 'Oracle', 'Popeyes', 3, 'Java');
